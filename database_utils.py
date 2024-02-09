07bc865f53ebea8ca71ac0f817cb1dc89aa7ebe0
Tomasz Karwala <tkarwala@rbcpolska.pl>
2024-02-09 23:18:00 +0100

    Code review v1

--- database_utils.py
+++ database_utils.py
@@ -2,14 +2,19 @@ import logging
 import pyodbc
 # "AzureWebJobsStorage": "DefaultEndpointsProtocol=https;AccountName=timescrappers;AccountKey=m/mAvEkfRoXQHUn6Kk/0oZZOXGjoTkkIjgBIK98hbYlWQZ/Wy8NCpvZi5DQim0yRriJirtruNbgG+AStWaLFiA==;EndpointSuffix=core.windows.net",
 def get_database_connection():
-    server = 'joboffers.database.windows.net'
-    database = 'JobOffersDB'
-    username = 'Nobody'
-    password = 'Karwala1'
+    server = 'joboffers.database.windows.net' #->config
+    database = 'JobOffersDB' #->config
+    username = 'Nobody' #->config
+    password = 'Karwala1' #->config
     driver = '{ODBC Driver 18 for SQL Server}'
     connection_string = f'DRIVER={driver};SERVER=tcp:{server};PORT=1433;DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
     return pyodbc.connect(connection_string)
 
+# rozbij na dwie funkcje: get_website, create_wepsite
+# logikę zakodź w funkcjach wyżej
+# nie przekazuj cursora jako parametru - on powinien być inicjalizowany
+# w każdej z funkcji połączenia do bazy
+# w każdej osobno powinieneś mieć try/ except
 def get_or_create_website(cursor, website_name, website_url):
     cursor.execute("SELECT id FROM api_websites WHERE Website_url = ?", (website_url))
     result = cursor.fetchone()
@@ -18,7 +23,7 @@ def get_or_create_website(cursor, website_name, website_url):
     else:
         cursor.execute("INSERT INTO api_websites (Website_name, Website_url) OUTPUT INSERTED.id VALUES (?, ?)", (website_name, website_url))
         return cursor.fetchone()[0]
-
+# jw
 def get_or_create_category(cursor, category_name):
     cursor.execute("SELECT id FROM api_categories WHERE Category_name = ?", (category_name,))
     result = cursor.fetchone()
@@ -28,6 +33,8 @@ def get_or_create_category(cursor, category_name):
         cursor.execute("INSERT INTO api_categories (Category_name) OUTPUT INSERTED.id VALUES (?)", (category_name,))
         return cursor.fetchone()[0]
 
+# logika pobierania website_id i category_id powinna być wyżej
+# funkcja powinna dostawać powyższe zmienne jako parametr
 def insert_offer_data(offer_data):
     conn = None
     cursor = None
@@ -44,7 +51,8 @@ def insert_offer_data(offer_data):
             return
 
         logging.info(f"Próba wstawienia danych oferty pracy: {offer_data['Position']}")
-
+        
+        #
         website_id = get_or_create_website(cursor, offer_data['Website_name'], offer_data['Website'])
         category_id = get_or_create_category(cursor, offer_data['Category'])
 
